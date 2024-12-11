2d86513f7dda986570cff2e94bb64ed96534f3b2
rasunM <rasunmanohara@gmail.com>
2024-12-11 16:32:51 +0530

    create a card component for destinations page

---
+++ src/app/destinations/components/card.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import { FaCircleChevronRight } from "react-icons/fa6";
+
+type CardProps = {
+    image: string;
+    title: string;
+    description: string;
+    link: string
+}
+
+export default function Card(){
+    return (
+        <div className="min-w-80 min-h- w-80  shadow-lg rounded-t-2xl rounded-b-lg">
+            <div className="">
+                <Image src={"/destination.jpg"} alt="" width={320} height={28} className="rounded-t-2xl"/>
+            </div>
+            <div className="mx-5">
+                <div className="font-bold text-xl my-9">Text Section</div>
+                <div className="my-9 text-gray-600">Whether you're a history buff, foodie, or seeking adventure, our curated selection of blogs, articles, and guides will help you plan the perfect holiday. We have mapped out the best things</div>
+            </div>
+            <div className="flex mx-5 my-10 items-center gap-3">
+                <div className="font-bold text-green-950">Explore</div>
+                <div><FaCircleChevronRight/></div>
+            </div>
+        </div>
+    )
+}
--- src/app/destinations/page.tsx
+++ src/app/destinations/page.tsx
@@ -1,6 +1,7 @@
 import CommonHeroSection from "../common/common_hero_section";
 import { RiArrowDropRightFill } from "react-icons/ri";
 import Image from "next/image";
+import Card from "./components/card";
 
 export default function Destinations(){
     return (
@@ -18,6 +19,16 @@ export default function Destinations(){
                 </div>
                 <div className="flex justify-center my-16">
                     <Image src={"/home_assests/map-image.jpeg"} alt="" width={500} height={500}/>
+                </div>
+                <div className="my-7">
+                    <div className="text-2xl font-semibold my-8">Top Destinations</div>
+                    <span className="text-gray-600 ">Whether you're a history buff, foodie, or seeking adventure, our curated selection of blogs, articles, and guides will help you plan the perfect holiday. We have mapped out the best things to do, attractions and experiences in Sri Lanka for each destination, so you can make the most of your trip. Start planning your dream holiday now!</span>
+                </div>
+                <div className="flex gap-5 flex-wrap justify-between items-start my-8">
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
                 </div>
             </div>
         </div>
