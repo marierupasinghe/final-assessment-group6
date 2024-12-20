4e1c254e60f52601d156d1f3ccb84786147a9d95
rasunM <rasunmanohara@gmail.com>
2024-12-20 11:24:17 +0530

    resolving isues

--- codebase/src/app/api/destination/[destination_tmp]/route.ts
+++ codebase/src/app/api/destination/[destination_tmp]/route.ts
@@ -2,13 +2,14 @@ import { sql } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
 
 // The function will handle GET requests and return destinations based on province
-export async function GET(request: NextRequest, context: { params: { destination_tmp: string } }) {
+export async function GET(request: NextRequest, { params }: { params: { destination_tmp: string } }) {
     try {
         // Destructure the destination_tmp from the params object
-        const { destination_tmp } =await context.params;
+        const { destination_tmp } = params;
 
         // Capitalize the first letter of destination_tmp
-        const formattedProvince = destination_tmp.charAt(0).toUpperCase() + destination_tmp.slice(1);
+        const formattedProvince =
+            destination_tmp.charAt(0).toUpperCase() + destination_tmp.slice(1);
 
         // Query the database to get all destinations with the specified province
         const reviews = await sql`
@@ -19,6 +20,9 @@ export async function GET(request: NextRequest, context: { params: { destination
         return NextResponse.json(reviews.rows.length > 0 ? reviews.rows : null);
     } catch (error) {
         console.error("Error fetching destinations:", error);
-        return NextResponse.json({ error: "Error fetching destinations" }, { status: 500 });
+        return NextResponse.json(
+            { error: "Error fetching destinations" },
+            { status: 500 }
+        );
     }
 }
