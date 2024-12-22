34952d0842e50b98596d47a1ce3973fe5fab61e4
rasunM <rasunmanohara@gmail.com>
2024-12-22 11:51:11 +0530

    resolve errors

--- codebase/src/app/login/page.tsx
+++ codebase/src/app/login/page.tsx
@@ -3,7 +3,10 @@ import { redirect } from "next/navigation";
 import Form from "./form";
 
 export default async function Login(){
-    
+    const session = await getServerSession();
+    if (session){
+        redirect("/");
+    }
     return (
         <Form/>
     )
