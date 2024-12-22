9d24c915ef087d576de673e06a5b84fe6ed6d4b6
rasunM <rasunmanohara@gmail.com>
2024-12-22 12:00:59 +0530

    resolve errors in server

--- codebase/src/app/login/page.tsx
+++ codebase/src/app/login/page.tsx
@@ -1,18 +1,11 @@
-import { useSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import Form from "./form";
 
-export default function UserProfile() {
-  const { data: session } = useSession();
-
-  if (!session) {
-    return <p>You are not logged in.</p>;
-  }
-
-  return (
-    <div>
-      <h1>Welcome, {session.user?.name}!</h1>
-      <p>Your email: {session.user?.email}</p>
-      <Form/>
-    </div>
-  );
+export default async function Login(){
+    const session = await getServerSession();
+    
+    return (
+        <Form/>
+    )
 }
