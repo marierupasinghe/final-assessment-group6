dea9e97e3026e199da5b7e44fe90da35ff968290
rasunM <rasunmanohara@gmail.com>
2024-12-22 11:31:01 +0530

    added the login page

--- codebase/src/app/api/auth/[...nextauth]/route.ts
+++ codebase/src/app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,45 @@
-import NextAuth from 'next-auth'
-import CredentialsProvider from "next-auth/providers/credentials"
-import { compare } from 'bcrypt' 
-import { sql } from '@vercel/postgres'
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { compare } from 'bcrypt';
+import { sql } from '@vercel/postgres';
 
 const handler = NextAuth({
-    session: {
-        strategy: 'jwt'
-    },
-    providers: [CredentialsProvider({
-        credentials: {
-            email: {},
-            password: {}
-          },
-          async authorize(credentials, req) {
-            //
-            const response = await sql `select * from users where email=${credentials?.email}`;
-            const user = response.rows[0];
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/login',
+  },
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials, req) {
+        //
+        const response = await sql`
+        SELECT * FROM users WHERE email=${credentials?.email}`;
+        const user = response.rows[0];
 
-            const passwordCorrect = await user.password == credentials?.password
-      
+        const passwordCorrect = await compare(
+          credentials?.password || '',
+          user.password
+        );
 
-            console.log(passwordCorrect)
-            console.log(credentials?.email)
-            console.log(credentials?.password)
-            console.log(user.password)
+        console.log({ passwordCorrect });
 
-            if (passwordCorrect){
-                return {
-                    id: user.id,
-                    email: user.email
-                }
-            }
-            return null;
-          }
-    })]
-})
+        if (passwordCorrect) {
+          return {
+            id: user.id,
+            email: user.email,
+          };
+        }
 
-export {handler as GET, handler as POST};
+        return null;
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
