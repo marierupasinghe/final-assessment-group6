6473126c845d29d0a87e308c357d82e8ebb155ec
rasunM <rasunmanohara@gmail.com>
2024-12-22 11:34:18 +0530

    resolve issues in login page

--- codebase/src/app/login/page.tsx
+++ codebase/src/app/login/page.tsx
@@ -3,10 +3,7 @@ import { redirect } from "next/navigation";
 import Form from "./form";
 
 export default async function Login(){
-    const session = await getServerSession();
-    if (session){
-        redirect("/");
-    }
+    
     return (
         <Form/>
     )
