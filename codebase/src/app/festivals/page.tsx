fed0606f4a6bba63ed4419d63b1e65c79f43c9e5
oshini2002 <160472890+oshini2002@users.noreply.github.com>
2024-12-24 20:34:38 +0530

    edit festival page

--- codebase/src/app/festivals/page.tsx
+++ codebase/src/app/festivals/page.tsx
@@ -24,15 +24,11 @@ Whether you're captivated by the rhythmic beats of traditional drummers, the gra
 Below, explore the most popular festivals celebrated month by month and discover what makes each occasion special.</span>
                 </div>
                 <div className="flex justify-center my-16">
-                    <Image src={"/home_assests/map-image.jpeg"} alt="" width={500} height={500}/>
+                    <Image src={"/home_assests/sl-map.jpg"} alt="" width={500} height={500}/>
                 </div>
                 <div className="my-7">
                     <div className="text-2xl font-semibold my-8">Top Festivals</div>
-                    <span className="text-gray-600 ">Sri Lanka is a land of vibrant cultural celebrations and religious observances that span across its diverse communities. From the grand processions of Buddhist peraheras to the colorful Hindu festivals, solemn Christian ceremonies, and joyous Sinhala and Tamil New Year, each month offers a unique experience. These festivals are deeply rooted in the country's history and traditions, showcasing its rich cultural heritage and spiritual essence.
-
-Whether you're captivated by the rhythmic beats of traditional drummers, the graceful movements of dancers, or the spiritual ambiance of sacred rituals, Sri Lanka's festivals are a window into the island's soul. Plan your visit to coincide with these extraordinary events and immerse yourself in the culture, colors, and rhythms that make Sri Lanka truly unforgettable.
-
-Below, explore the most popular festivals celebrated month by month and discover what makes each occasion special.</span>
+                    <span className="text-gray-600 ">"Sri Lanka is a treasure trove of vibrant cultural festivities and sacred traditions, celebrated with passion and reverence by its diverse communities. From the majestic Buddhist peraheras to lively Hindu festivals, solemn Christian observances, and the joyous Sinhala and Tamil New Year, every month brings unique celebrations that reflect the island's rich heritage. These events, steeped in history and tradition, offer a glimpse into Sri Lanka's spiritual and cultural essence. Whether it’s the mesmerizing rhythms of traditional drummers, the elegance of dancers, or the serene atmosphere of religious rituals, Sri Lanka’s festivals are an invitation to experience the heart of the island. Plan your journey to align with these remarkable celebrations and immerse yourself in the vibrant culture, traditions, and timeless beauty of Sri Lanka. Below, discover a month-by-month guide to the most iconic festivals and what makes them truly special."</span>
                 </div>
                 <div className="flex gap-5 flex-wrap justify-between items-start my-8">
                     {
